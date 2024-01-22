36105d959c87ee67c0825ceab6b7a9bd57a836b5
Harmonyy1 <gsennerblom@gmail.com>
2024-01-22 13:18:26 +0100

    done part 2

--- förstaförsöket.php
+++ förstaförsöket.php
@@ -1,11 +1,39 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-    <?php
+    <style>
 
-        $halloj = "Hejsan!";
-        echo $halloj;
+        .hehe{
+            color: red;
+            font-size: 50px;
+        }
+
+    </style>
+
+</head>
+<body>
+    
+
+
+
+
+    <div class="hehe">
+        <?php $halloj = "Hello World!";
+        echo $halloj ?>
+    </div>    
+
+
 
     
-    ?>
+    
 
-</html
+    
+
+
+
+</body>
+</html>
