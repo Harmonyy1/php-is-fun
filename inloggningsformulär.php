81e4a58cd7e207b200789a619e81a1374e8badd7
Gabbe <gsennerblom@gmail.com>
2024-05-18 15:44:41 +0200

    wedding time

---
+++ inloggningsformulär.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="formbox">
+        <form action="login.php" method="post" id="frmLogin">
+            <input type="text" name="username" id="username" placeholder="Username">
+            <input type="password" name="password" id="password">
+            <input type="submit" name="btnLogin" id="btnLogin" value="Login">
+        </form>
+    </div>
+</body>
+</html>
